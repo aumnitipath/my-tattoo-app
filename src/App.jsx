73852ee28d7cec71f2fbbd7fe674ddf8a89cc628
aumnitipath <aumnitipath@outlook.com>
2023-07-31 04:02:48 +0700

    add:data and contents

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,8 @@ import "./App.css";
 import AppHeader from "./components/AppHeader";
 import TattooPost from "./components/TattoPost";
 import TattooItem from "./components/TattooItem";
+import tattoos from "./data/tattoos";
 
-const tattoos = [
-  {
-    title: "Arm OldSchool Tattoo",
-    thumbnailUrl: "/images/tattoo-01-small.jpg",
-  },
-  {
-    title: "Hand OldSchool Tattoo",
-    thumbnailUrl: "/images/tattoo-02-small.jpg",
-  },
-  {
-    title: "Neck NewSchool Tattoo",
-    thumbnailUrl: "/images/tattoo-03-small.jpg",
-  },
-  {
-    title: "Back OldSchool Tattoo",
-    thumbnailUrl: "/images/tattoo-04-small.jpg",
-  },
-];
 function App() {
   const tattooElements = tattoos.map((tattoo, index) => {
     return <TattooItem key={index} tattoo={tattoo} />;
---
+++ src/data/tattoos.jsx
@@ -0,0 +1,28 @@
+const tattoos = [
+  {
+    title: "Arm OldSchool Tattoo",
+    thumbnailUrl: "/images/tattoo-01-small.jpg",
+  },
+  {
+    title: "Hand OldSchool Tattoo",
+    thumbnailUrl: "/images/tattoo-02-small.jpg",
+  },
+  {
+    title: "Neck NewSchool Tattoo",
+    thumbnailUrl: "/images/tattoo-03-small.jpg",
+  },
+  {
+    title: "Back OldSchool Tattoo",
+    thumbnailUrl: "/images/tattoo-04-small.jpg",
+  },
+  {
+    title: "Back Arm OldSchool Tattoo",
+    thumbnailUrl: "/images/tattoo-05-small.jpg",
+  },
+  {
+    title: "Font Arm OldSchool Tattoo",
+    thumbnailUrl: "/images/tattoo-06-small.jpg",
+  },
+];
+
+export default tattoos;
