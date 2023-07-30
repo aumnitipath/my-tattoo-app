44ab8ee5b2ae0085823dc7493065b571d0066305
aumnitipath <aumnitipath@outlook.com>
2023-07-31 00:34:01 +0700

    add:head and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <header className="app-header">
+          <img className="app-header-logo" src="/images/logo.png"></img>
+        </header>
+        <div className="app-grid">
+          <div className="tattoo-item">
+            <img src="/images/tattoo-01-small.jpg" />
+            <h4>ต่ายน้อย 1</h4>
+          </div>
+
+          <div className="tattoo-item">
+            <img src="/images/tattoo-02-small.jpg" />
+            <h4>ต่ายน้อย 1</h4>
+          </div>
+
+          <div className="tattoo-item">
+            <img src="/images/tattoo-03-small.jpg" />
+            <h4>ต่ายน้อย 1</h4>
+          </div>
+
+          <div className="tattoo-item">
+            <img src="/images/tattoo-04-small.jpg" />
+            <h4>ต่ายน้อย 1</h4>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
