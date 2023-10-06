1d1f32282e4f66ad50d641653cc4de72fc457962
aumnitipath <aumnitipath@outlook.com>
2023-10-07 03:56:56 +0700

    fix:data to lowercase

--- src/data/tattoos.jsx
+++ src/data/tattoos.jsx
@@ -1,31 +1,31 @@
 let tattoos = [
   {
-    title: "Arm OldSchool Tattoo",
+    title: "arm oldSchool Tattoo",
     thumbnailUrl: "/images/tattoo-01-small.jpg",
     fullUrl: "/images/tattoo-01-large.jpg",
   },
   {
-    title: "Hand OldSchool Tattoo",
+    title: "hand OldSchool Tattoo",
     thumbnailUrl: "/images/tattoo-02-small.jpg",
     fullUrl: "/images/tattoo-02-large.jpg",
   },
   {
-    title: "Neck NewSchool Tattoo",
+    title: "neck NewSchool Tattoo",
     thumbnailUrl: "/images/tattoo-03-small.jpg",
     fullUrl: "/images/tattoo-03-large.jpg",
   },
   {
-    title: "Back OldSchool Tattoo",
+    title: "back OldSchool Tattoo",
     thumbnailUrl: "/images/tattoo-04-small.jpg",
     fullUrl: "/images/tattoo-04-large.jpg",
   },
   {
-    title: "Back Arm OldSchool Tattoo",
+    title: "back Arm OldSchool Tattoo",
     thumbnailUrl: "/images/tattoo-05-small.jpg",
     fullUrl: "/images/tattoo-05-large.jpg",
   },
   {
-    title: "Font Arm OldSchool Tattoo",
+    title: "font Arm OldSchool Tattoo",
     thumbnailUrl: "/images/tattoo-06-small.jpg",
     fullUrl: "/images/tattoo-06-large.jpg",
   },
